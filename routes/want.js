26efd077653e02639ac06f73719635dde2006e27
yuhui44 <yuhui44@live.cn>
2018-04-14 13:12:45 +0800

    4.14.13.12

--- routes/want.js
+++ routes/want.js
@@ -20,16 +20,4 @@ router.get('/wants', checkToken, WantController.getWants);
 router.get('/needs', checkToken, WantController.getNeeds);
 // 管理员查询所有的意向信息
 router.get('/allWants', checkToken, checkAdmin, WantController.getAllWants);
-// // 新建或编辑产权信息 请求：_id（编辑）、propertyName、summary、detail、isPublish、isSelt、isDisabled、createTime、editTime
-// router.post('/property', checkToken, checkEmail, checkDisabled, PropertyController.postProperty);
-// // 获取产权信息详情 请求：_id
-// router.get('/property', checkToken, PropertyController.getProperty);
-// // 获取产权信息列表
-// router.get('/propertys', checkToken, PropertyController.getPropertys);
-// // 管理员获取所有产权信息列表
-// router.get('/allPropertys', checkToken, checkEmail, checkDisabled, checkAdmin, PropertyController.getAllPropertys);
-// // 首页获取产权列表
-// router.get('/indexPropertys', PropertyController.indexPropertys);
-// // 首页获取产权详情 参数：_id
-// router.get('/indexProperty', PropertyController.indexProperty);
 module.exports = router
