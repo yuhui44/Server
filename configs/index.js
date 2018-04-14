ba424c53cfdac8f6364445f2238d0b175b1e2381
yuhui44 <yuhui44@live.cn>
2018-04-14 20:47:24 +0800

    4.14.20.47

--- configs/index.js
+++ configs/index.js
@@ -2,11 +2,11 @@
 
 let config = {
   // 验证邮箱邮件中的网址前缀
-  checkEmailFontLink: 'http://localhost:8080/valiEmail/',
+  checkEmailFontLink: 'http://localhost/valiEmail/',
   // 重置密码邮件中的网址前缀
-  resetPasswordFontLink: 'http://localhost:8080/resetPass/',
+  resetPasswordFontLink: 'http://localhost/resetPass/',
   // 用户个人中心-我的转让网站
-  myPropertysLink: 'http://localhost:8080/user/property',
+  myPropertysLink: 'http://localhost/user/property',
   // 发送邮件的账号
   email: {
     service: '163',
@@ -30,14 +30,14 @@ let config = {
   // mongoDB数据库配置
   mongodb: {
     host: '127.0.0.1',
-    database: 'vue-login',
+    database: 'idea',
     port: 27017,
     user: '',
     password: ''
   },
   // 服务端接口配置
   app: {
-    port: process.env.PORT || 8888,
+    port: process.env.PORT || 80,
     routerBaseApi: '/api'
   }
 };
